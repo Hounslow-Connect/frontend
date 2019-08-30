1db8e7427fd2a9e2316046e1dc03385c43e25b3a
jaderyan <jadeelizabethryan@outlook.com>
2019-08-30 14:25:17 +0100

    add font awesome library

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
 import React from 'react';
 import './App.scss';
+
+// add all free font awesome icons to project
+
+library.add(fas);
 
 const App: React.FunctionComponent = () => (
   <div>
