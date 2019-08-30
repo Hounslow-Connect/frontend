0aa6c0c342e3a2c6e7665fcce5c85b60a2b82ea7
jaderyan <jadeelizabethryan@outlook.com>
2019-08-30 13:14:18 +0100

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.scss';
+
+const App: React.FunctionComponent = () => (
+  <div>
+    <h1 className="example">Hello World!</h1>
+  </div>
+);
+
+export default App;
