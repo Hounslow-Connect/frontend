cd6faac5f5f96b23ca9593b6695e3faed0365814
jaderyan <jadeelizabethryan@outlook.com>
2019-09-02 08:44:49 +0100

    Link component and styles

---
+++ src/components/Link/Link.tsx
@@ -0,0 +1,41 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { IconName } from '@fortawesome/fontawesome-common-types';
+import cx from 'classnames';
+import React from 'react';
+
+import './Link.scss';
+
+interface ILinkProps {
+  text: string;
+  href: string;
+  icon?: IconName;
+  iconPosition?: 'left' | 'right';
+  size: 'large' | 'medium';
+  inline?: boolean;
+}
+
+const Link: React.FunctionComponent<ILinkProps> = ({
+  text,
+  href,
+  size,
+  inline,
+  icon,
+  iconPosition,
+}) => (
+  <a
+    href={href}
+    className={cx('link', `link--${size}`, {
+      ['link__inline']: inline,
+    })}
+  >
+    {icon && iconPosition === 'left' && (
+      <FontAwesomeIcon icon={icon} className="link__icon--left" />
+    )}
+    {text}
+    {icon && iconPosition === 'right' && (
+      <FontAwesomeIcon icon={icon} className="link__icon--right" />
+    )}
+  </a>
+);
+
+export default Link;
