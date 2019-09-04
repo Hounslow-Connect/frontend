5652fdb1e05c7bb874c36225197fcc57aea5c24d
jaderyan <jadeelizabethryan@outlook.com>
2019-09-04 16:33:56 +0100

    move burger menu to own component

---
+++ src/components/BurgerMenu/BurgerMenu.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Link from '../Link';
+import Button from '../Button';
+
+import './BurgerMenu.scss';
+
+const BurgerMenu = () => (
+  <div className="header_burger-menu">
+    <Link text="Home" href="#" size="large" />
+    <Link text="About" href="#" size="large" />
+    <Link text="Contact Us" href="#" size="large" />
+    <Link text="Get Involved" href="#" size="large" />
+    <Button text="Translate" size="small" burgerMenu={true} icon="language" />
+    <Button text="Give feedback" size="small" burgerMenu={true} icon="comment" />
+    <Button text="View favourites" size="small" burgerMenu={true} icon="star" />
+  </div>
+);
+
+export default BurgerMenu;
