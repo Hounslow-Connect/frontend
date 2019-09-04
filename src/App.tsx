43f623a549f11cc1c36c1bdcf4cce995b242feea
jaderyan <jadeelizabethryan@outlook.com>
2019-09-04 15:18:34 +0100

    add windowsize store for tracking mobile sizes

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,43 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider, observer } from 'mobx-react';
 
 import Home from './views/Home';
 import NotFound from './views/NotFound';
 import Results from './views/Results';
 
-import React, { Fragment } from 'react';
+import React, { Component } from 'react';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header';
 // add all free font awesome icons to project
 
 import './styles/grid.scss';
+import WindowSizeStore from './stores/windowSizeStore';
 
 library.add(fas);
+const windowSizeStore = new WindowSizeStore();
 
-const App: React.FunctionComponent = () => (
-  <Fragment>
-    <Header />
-    <Router>
-      <Switch>
-        <Route path="/" exact={true} component={Home} />
-        <Route path="/results" component={Results} />
-        <Route component={NotFound} />
-      </Switch>
-    </Router>
-    <Footer />
-  </Fragment>
-);
+class App extends Component {
+  componentDidMount() {
+    windowSizeStore.setWindow();
+  }
 
-export default App;
+  render() {
+    return (
+      <Provider windowSizeStore={windowSizeStore}>
+        <Header />
+        <Router>
+          <Switch>
+            <Route path="/" exact={true} component={Home} />
+            <Route path="/results" component={Results} />
+            <Route component={NotFound} />
+          </Switch>
+        </Router>
+        <Footer />
+      </Provider>
+    );
+  }
+}
+
+export default observer(App);
