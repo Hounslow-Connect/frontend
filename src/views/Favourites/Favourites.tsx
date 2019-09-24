a164340a637260320764b99ff57453e634ea8ff1
jaderyan <jadeelizabethryan@outlook.com>
2019-09-24 08:51:34 +0100

    favourites styling for desktop

--- src/views/Favourites/Favourites.tsx
+++ src/views/Favourites/Favourites.tsx
@@ -1,52 +1,66 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Button from '../../components/Button';
-import find from 'lodash/find';
 import { inject, observer } from 'mobx-react';
 
 import { IService } from '../../types/types';
 
 import FavouritesCard from './FavouritesCard';
 
-const Favourites: React.FunctionComponent<any> = ({ favouritesStore }) => (
-  <main>
-    <header>
-      <div>
-        <h1>Favourites</h1>
-        <div>You can print or share a list of your favourite services</div>
-      </div>
-      <div>
-        <div>
-          <p>Share</p>
-          <FontAwesomeIcon icon={['fab', 'facebook-f']} />
-          <FontAwesomeIcon icon={['fab', 'twitter']} />
-          <FontAwesomeIcon icon="link" />
-          <FontAwesomeIcon icon="envelope" />
-        </div>
-        <Button text="Print page" icon="print" alt={true} size="small" />
-      </div>
-    </header>
-    {favouritesStore.favourites.length && (
-      <section>
-        {favouritesStore.favourites.map((favourite: IService) => {
-          const organisation = find(favouritesStore.organisations, [
-            'id',
-            favourite.organisation_id,
-          ]);
+import './Favourites.scss';
 
-          return (
-            <FavouritesCard
-              key={favourite.id}
-              service={favourite}
-              organisation={organisation}
-              locations={favouritesStore.getLocations(favourite.id)}
-              removeFavourite={favouritesStore.removeFavourite}
-            />
-          );
-        })}
-      </section>
-    )}
-  </main>
+const Favourites: React.FunctionComponent<any> = ({ favouritesStore }) => (
+  <section>
+    <div className="favourites__header flex-container">
+      <div className="flex-col flex-col--12 favourites__header--heading">
+        <h1>Favourites</h1>
+      </div>
+      <div className="flex-container flex-container--mobile-no-padding flex-container--align-center favourites__header--inner-container">
+        <div className="flex-col flex-col--6 flex-col--mobile--12 flex-col--tablet-large--5 favourites__header--header-container">
+          <p>You can print or share a list of your favourite services</p>
+        </div>
+        <div className="flex-col flex-col--5 flex-col--tablet-large--6 mobile-hide tablet-hide favourites__header--header-container">
+          <div className="flex-container flex-container--align-center">
+            <div className="flex-col flex-col--8 flex-col--tablet-large--7 favourites__header--share">
+              <p>Share</p>
+              <FontAwesomeIcon icon={['fab', 'facebook-f']} />
+              <FontAwesomeIcon icon={['fab', 'twitter']} />
+              <FontAwesomeIcon icon="link" />
+              <FontAwesomeIcon icon="envelope" />
+            </div>
+            <div className="flex-col flex-col--4 flex-col--tablet-large--5 favourites__header--print">
+              <Button text="Print page" icon="print" alt={true} size="small" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div className="flex-container flex-container--mobile-no-padding">
+      {!!favouritesStore.favourites.length && (
+        <Fragment>
+          <div className="flex-col flex-col--12 favourites__count">
+            <p>{`${favouritesStore.favourites.length} results found`}</p>
+          </div>
+
+          <div className="flex-col flex-col--12">
+            <div className="flex-container flex-container--justify">
+              {favouritesStore.favourites.map((favourite: IService) => {
+                return (
+                  <FavouritesCard
+                    key={favourite.id}
+                    service={favourite}
+                    locations={favouritesStore.getLocations(favourite.id)}
+                    removeFavourite={favouritesStore.removeFavourite}
+                  />
+                );
+              })}
+            </div>
+          </div>
+        </Fragment>
+      )}
+    </div>
+  </section>
 );
 
 export default inject('favouritesStore')(observer(Favourites));
--- src/views/Favourites/FavouritesCard.tsx
+++ src/views/Favourites/FavouritesCard.tsx
@@ -1,84 +1,126 @@
 import React from 'react';
 import first from 'lodash/first';
+import find from 'lodash/find';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import { IService, IOrganisation } from '../../types/types';
+import { IService } from '../../types/types';
 import { getLocationName } from '../../utils/utils';
 import Accordian from '../../components/Accordian';
 
 import { apiBase } from '../../config/api';
+import { observer, inject } from 'mobx-react';
+import FavouritesStore from '../../stores/favouritesStore';
+import get from 'lodash/get';
+import { withRouter, RouteComponentProps } from 'react-router';
 
-interface IProps {
+interface IProps extends RouteComponentProps {
   service: IService;
-  organisation: IOrganisation;
   locations: [];
   removeFavourite: (id: string) => void;
+  favouritesStore?: FavouritesStore;
 }
+
 const FavouritesCard: React.FunctionComponent<IProps> = ({
   service,
   locations,
   removeFavourite,
+  favouritesStore,
+  history,
 }) => {
   const serviceLocations = getLocationName(locations);
+
+  if (!favouritesStore) {
+    return null;
+  }
+
+  const organisation = find(favouritesStore.organisations, ['id', service.organisation_id]);
+
   return (
-    <article>
-      <div>
-        <div>
-          <h2>{service.name}</h2>
-          {!!serviceLocations.length && (
-            <div className="flex search-result-card__location">
-              <FontAwesomeIcon icon="map-marker-alt" />
-              {serviceLocations.length === 1 ? (
-                <p>{first(serviceLocations)}</p>
-              ) : (
-                <Accordian
-                  title={`${serviceLocations.length} locations`}
-                  className={'search-result-card__location-list'}
-                >
-                  {serviceLocations.map(location => (
-                    <p key={`${service.id}-${location}`}>{location}</p>
-                  ))}
-                </Accordian>
+    <article className="flex-col flex-col--4">
+      <div className="favourites__card">
+        <div className="flex-container favourites__card--inner">
+          <div className="flex-col flex-col--12">
+            <div className="flex-container favourites__card--no-padding">
+              <div className="flex-col flex-col--8">
+                <h3>{service.name}</h3>
+                <p className="body--s favourites__card--organisation">
+                  {get(organisation, 'name')}
+                </p>
+                <div className="search-result-card__location favourites__card--location">
+                  <FontAwesomeIcon icon="map-marker-alt" />
+                  {serviceLocations.length === 1 ? (
+                    <h4>{first(serviceLocations)}</h4>
+                  ) : (
+                    <Accordian
+                      title={`${serviceLocations.length} locations`}
+                      className={'search-result-card__location-list'}
+                    >
+                      {serviceLocations.map(location => (
+                        <h4 key={`${location.id}-${location}`}>{location}</h4>
+                      ))}
+                    </Accordian>
+                  )}
+                </div>
+              </div>
+              {service.has_logo && (
+                <div className="flex-col flex-col--4 favourites__logo">
+                  <img
+                    src={
+                      service.has_logo
+                        ? `${apiBase}/services/${service.id}/logo.png?v=${service.updated_at}`
+                        : `${apiBase}/organisations/${service.organisation_id}/logo.png?v=${service.updated_at}`
+                    }
+                    alt={service.name}
+                  />
+                </div>
               )}
             </div>
-          )}
+            <div className="flex-col flex-col--12">
+              <p className="body--s favourites__card--intro">{service.intro}</p>
+            </div>
+            <div className="flex-container favourites__card--contact">
+              <div className="flex-col flex-col--12">
+                <p className="favourites__card--contact--heading">
+                  <FontAwesomeIcon icon="phone" /> Telephone
+                </p>
+                <p className="body--s">{service.contact_phone}</p>
+              </div>
+              <div className="flex-col flex-col--12">
+                <p className="favourites__card--contact--heading">
+                  <FontAwesomeIcon icon="envelope" /> Email
+                </p>
+                <p className="body--s">{service.contact_email}</p>
+              </div>
+            </div>
+          </div>
         </div>
-        <div>
-          <img
-            src={
-              service.has_logo
-                ? `${apiBase}/services/${service.id}/logo.png?v=${service.updated_at}`
-                : `${apiBase}/organisations/${service.organisation_id}/logo.png?v=${service.updated_at}`
-            }
-            alt={service.name}
-          />
-        </div>
-      </div>
-      <div>{service.intro}</div>
-      <div>
-        <p>
-          <FontAwesomeIcon icon="phone" /> Telephone
-        </p>
-        <p> {service.contact_phone}</p>
-        <p>
-          <FontAwesomeIcon icon="envelope" /> Email
-        </p>
-        <p>{service.contact_email}</p>
-      </div>
-      <div>
-        <div role="button" onClick={() => removeFavourite(service.id)}>
-          <p>
-            Remove <FontAwesomeIcon icon="times" />
-          </p>
-        </div>
-        <div>
-          <p>
-            Remove <FontAwesomeIcon icon="chevron-right" />
-          </p>
+        <div className="flex-col flex-col--12 favourites__card--button-row">
+          <div className="flex-container favourites__card--no-padding">
+            <div
+              className="flex-col flex-col--6 favourites__card--button"
+              role="button"
+              aria-label="Remove favourite"
+              onClick={() => removeFavourite(service.id)}
+            >
+              <p>
+                Remove <FontAwesomeIcon icon="times" />
+              </p>
+            </div>
+            <div
+              className="flex-col flex-col--6 favourites__card--button favourites__card--button--more"
+              role="navigation"
+              aria-label={`View more information about ${service.name}`}
+              onClick={() => history.push(`/service/${service.slug}`)}
+            >
+              <p>
+                View More <FontAwesomeIcon icon="chevron-right" />
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </article>
   );
 };
 
-export default FavouritesCard;
+export default inject('favouritesStore')(withRouter(observer(FavouritesCard)));
