75508b7526ff83ce436960015968fa1a7a77faa4
jaderyan <jadeelizabethryan@outlook.com>
2019-09-25 09:26:28 +0100

    step 3 added to flow

--- src/views/Referral/Referral.tsx
+++ src/views/Referral/Referral.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { inject, observer } from 'mobx-react';
 import queryString from 'query-string';
 import { withRouter, RouteComponentProps } from 'react-router';
@@ -14,6 +14,7 @@ import Button from '../../components/Button';
 import { apiBase } from '../../config/api';
 import StepOne from './Steps/StepOne';
 import StepTwo from './Steps/StepTwo';
+import StepThree from './Steps/StepThree';
 
 interface IProps extends RouteComponentProps {
   referralStore?: ReferralStore;
@@ -43,6 +44,8 @@ class Referral extends Component<IProps> {
         return <StepOne name={get(referralStore, 'service.name')} />;
       case 2:
         return <StepTwo name={get(referralStore, 'service.name')} />;
+      case 3:
+        return <StepThree />;
       default:
         break;
     }
@@ -127,42 +130,63 @@ class Referral extends Component<IProps> {
         </div>
 
         {referralStore.step === 1 && (
-          <div className="flex-col flex-col--mobile--12 mobile-show tablet-show">
-            <div className="flex-container flex-container--align-center">
-              <div className="flex-col flex-col--mobile--12 referral--form-time">
-                <div className="flex-container flex-container--align-center flex-container--mobile-no-padding">
-                  <div className="flex-col flex-col--mobile--2">
-                    <FontAwesomeIcon icon="clock" />
-                  </div>
-                  <div className="flex-col flex-col--mobile--10">
-                    <p>
-                      This form should take no longer than <strong>5 minutes</strong> to complete.
-                    </p>
+          <Fragment>
+            <div className="flex-col flex-col--mobile--12 mobile-show tablet-show">
+              <div className="flex-container flex-container--align-center">
+                <div className="flex-col flex-col--mobile--12 referral--form-time">
+                  <div className="flex-container flex-container--align-center flex-container--mobile-no-padding">
+                    <div className="flex-col flex-col--mobile--2">
+                      <FontAwesomeIcon icon="clock" />
+                    </div>
+                    <div className="flex-col flex-col--mobile--10">
+                      <p>
+                        This form should take no longer than <strong>5 minutes</strong> to complete.
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
+            <div className="flex-col flex-col--12 flex-col--mobile--12 mobile-show tablet-show">
+              <div className="flex-container referral--next-step referral--intro--no-padding">
+                <div className="flex-col flex-col--12 flex-col--mobile--12">
+                  <Button
+                    text="Continue"
+                    type="submit"
+                    icon="chevron-right"
+                    onClick={() => referralStore.nextStep()}
+                  />
+                </div>
+                <div className="flex-col flex-col--12 referral--step">
+                  <span
+                    className="body--s"
+                    dangerouslySetInnerHTML={{ __html: referralStore.stepDescription }}
+                  />
+                </div>
+              </div>
+            </div>
+          </Fragment>
         )}
 
-        <div className="flex-col flex-col--12 flex-col--mobile--12">
-          <div className="flex-container referral--next-step referral--intro--no-padding">
-            <div className="flex-col flex-col--12 flex-col--mobile--12">
-              <Button
-                text="Continue"
-                icon="chevron-right"
-                onClick={() => referralStore.nextStep()}
-                disabled={referralStore.step === 2 && !referralStore.whoFor}
-              />
-            </div>
-            <div className="flex-col flex-col--12 referral--step">
-              <span
-                className="body--s"
-                dangerouslySetInnerHTML={{ __html: referralStore.stepDescription }}
-              ></span>
+        {referralStore.step === 1 && (
+          <div className="flex-col flex-col--12 flex-col--mobile--12 mobile-hide tablet-hide">
+            <div className="flex-container referral--next-step referral--intro--no-padding">
+              <div className="flex-col flex-col--12 flex-col--mobile--12">
+                <Button
+                  text="Continue"
+                  icon="chevron-right"
+                  onClick={() => referralStore.nextStep()}
+                />
+              </div>
+              <div className="flex-col flex-col--12 referral--step">
+                <span
+                  className="body--s"
+                  dangerouslySetInnerHTML={{ __html: referralStore.stepDescription }}
+                ></span>
+              </div>
             </div>
           </div>
-        </div>
+        )}
       </div>
     );
   }
