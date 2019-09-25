7ee39e796fbae80ea637a604dfb2a7fcd3a71f12
jaderyan <jadeelizabethryan@outlook.com>
2019-09-25 20:50:02 +0100

    cms heading

---
+++ src/components/CMSPageLayout/CMSPage.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './CMSPage.scss';
+
+interface IProps {
+  title: string;
+  children: any;
+}
+
+const CMSPage: React.FunctionComponent<IProps> = ({ title, children }) => (
+  <section>
+    <div className="cms__header">
+      <div className="flex-container flex-container--mobile-no-padding">
+        <div className="flex-col flex-col--5">
+          <div className="cms__title-card">
+            <h1 className="cms__title-card--title">{title}</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+    <main className="flex-container">{children}</main>
+  </section>
+);
+
+export default CMSPage;
