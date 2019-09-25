26f1413d1e069dc606f71a2db2751a44574a78a8
jaderyan <jadeelizabethryan@outlook.com>
2019-09-25 13:01:59 +0100

    add errors showing for input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -5,6 +5,7 @@ import cx from 'classnames';
 interface IProps {
   placeholder?: string;
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  onBlur?: any;
   id: string;
   value: string;
   className?: string;
@@ -12,6 +13,8 @@ interface IProps {
   required?: boolean;
   type?: 'text' | 'email' | 'tel';
   pattern?: string;
+  error?: boolean;
+  errorMessage?: string;
 }
 
 const Input: React.FunctionComponent<IProps> = ({
@@ -24,22 +27,35 @@ const Input: React.FunctionComponent<IProps> = ({
   required,
   type = 'text',
   pattern,
+  onBlur,
+  error,
+  errorMessage,
 }) => (
-  <input
-    className={cx(
-      {
-        'input--full-width': fullWidth,
-      },
-      `input ${className}`
-    )}
-    placeholder={placeholder}
-    onChange={onChange}
-    id={id}
-    value={value}
-    required={required}
-    type={type}
-    pattern={pattern}
-  />
+  <div className={cx({ 'input--error': error })}>
+    <input
+      className={cx(
+        {
+          'input--full-width': fullWidth,
+        },
+        `input ${className}`
+      )}
+      placeholder={placeholder}
+      onChange={onChange}
+      id={id}
+      value={value}
+      required={required}
+      type={type}
+      pattern={pattern}
+      onBlur={onBlur}
+    />
+    <p
+      className={cx('input__error-message', {
+        'input__error-message--show': error,
+      })}
+    >
+      {errorMessage}
+    </p>
+  </div>
 );
 
 export default Input;
