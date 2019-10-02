d18737dd91d3caf54cf2ad25347fb4b09db243c9
jaderyan <jadeelizabethryan@outlook.com>
2019-10-02 17:33:37 +0100

    add margin to top of video

--- src/views/About.tsx
+++ src/views/About.tsx
@@ -21,7 +21,7 @@ const About: React.FunctionComponent<IProps> = ({ cmsStore }) => {
       <ReactMarkdown source={get(cmsStore, 'about.content')} />
       <ReactPlayer
         url={get(cmsStore, 'about.video_url')}
-        style={{ borderRadius: '19px', margin: 'auto' }}
+        style={{ borderRadius: '19px', margin: 'auto', marginTop: '24px' }}
         width={'90%'}
         light={true}
       />
