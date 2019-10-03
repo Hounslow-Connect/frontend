e67461e4e080f530b06ae325adfb6a7c0cdbccbf
jaderyan <jadeelizabethryan@outlook.com>
2019-10-03 13:57:04 +0100

    add fallback logo

--- src/views/Service/OrganisationCard/OrganisationCard.tsx
+++ src/views/Service/OrganisationCard/OrganisationCard.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import get from 'lodash/get';
 import cx from 'classnames';
 
+import FallBackLogo from '../../../assets/images/logo-fallback.png';
+
 import { apiBase } from '../../../config/api';
 import { IService } from '../../../types/types';
 
@@ -16,12 +18,14 @@ const OrganisationCard: React.FunctionComponent<IProps> = ({ service, sidebar })
       'flex-container--align-center service__organisation--sidebar': sidebar,
     })}
   >
-    <div className="flex-col flex-col--3 flex-col--mobile--12 service__organisation service__organisation--logo">
-      {get(service, 'organisation.has_logo') && (
+    <div className="flex-col flex-col--4 flex-col--mobile--12 service__organisation service__organisation--logo">
+      {get(service, 'organisation.has_logo') ? (
         <img
           src={`${apiBase}/organisations/${service.organisation_id}/logo.png?v=${service.updated_at}&max_dimension=100`}
           alt={`${get(service, 'organisation.name')} Logo`}
         />
+      ) : (
+        <img src={FallBackLogo} style={{ width: 100 }} />
       )}
     </div>
     <div className="flex-col flex-col--8 flex-col--mobile--12 service__organisation">
