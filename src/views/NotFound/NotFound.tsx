3b6d725e08acf7c585a51d60259a67890b413157
jaderyan <jadeelizabethryan@outlook.com>
2020-03-30 11:09:58 +0100

    change copy

--- src/views/NotFound/NotFound.tsx
+++ src/views/NotFound/NotFound.tsx
@@ -6,7 +6,7 @@ const NotFound: React.FunctionComponent = () => (
   <div className="flex-container flex-container--align-center not__found ">
     <div className="flex-col flex-col--12">
       <h1 className="not__found--heading">Page not found</h1>
-      <p className="body-l">What’s worse, a hilarious 404 page can’t be found either.</p>
+      <p className="body-l">It seems like the page you are looking for doesn't exist</p>
     </div>
   </div>
 );
