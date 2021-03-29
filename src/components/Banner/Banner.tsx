d5f1422a6589eec51c7541a27509bc8bf2d0a205
seanjessen <info@seanjessen.co.uk>
2021-03-29 17:29:18 +0100

    Added carousel to banner component

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,5 +1,8 @@
 import React, { FunctionComponent } from 'react';
 import ReactMarkdown from 'react-markdown';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import Button from '../Button';
 
 import './Banner.scss';
 import { IBanner } from '../../types/types';
@@ -16,6 +19,34 @@ const Banner: FunctionComponent<IProps> = ({ banner }) => (
           <div className="flex-col--12 banner__content">
             <h1 className="banner__title">{banner.title}</h1>
             <ReactMarkdown className="banner__row" source={banner.content} />
+          </div>
+
+          <div className="banner__carousel">
+            <div className="slides">
+              <div className="slide">
+                <div className="content">
+                  <h3 className="title">Current Campaigns</h3>
+                  <p>One Houslow Connect is an information, advice and guidance hub connecting local residents to local support.</p>
+                </div>
+                <Button
+                  text="Read more"
+                  icon="arrow-right"
+                  type="button"
+                />
+              </div>
+            </div>
+            <div className="arrows">
+              <button className="arrow arrow-left">
+                <FontAwesomeIcon
+                  icon="chevron-left"
+                />
+              </button>
+              <button className="arrow arrow-right">
+                <FontAwesomeIcon
+                  icon="chevron-right"
+                />
+              </button>
+            </div>
           </div>
         </div>
       </div>
