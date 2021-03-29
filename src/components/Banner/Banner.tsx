9415db905839e8e5f93376e009039f01a0bd431e
seanjessen <info@seanjessen.co.uk>
2021-03-29 12:29:02 +0100

    Amended banner styling on homepage

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -3,8 +3,6 @@ import ReactMarkdown from 'react-markdown';
 
 import './Banner.scss';
 import { IBanner } from '../../types/types';
-import ButtonLink from '../Button/ButtonLink';
-import { apiBase } from '../../config/api';
 
 interface IProps {
   banner: IBanner;
@@ -15,18 +13,10 @@ const Banner: FunctionComponent<IProps> = ({ banner }) => (
     <div className="flex-container flex-container--justify flex-container--mobile-no-padding">
       <div className="flex-col--12 banner__container">
         <div className="flex-container flex-container--no-padding">
-          <div className="flex-col--6 flex-col--mobile--12 banner__content">
+          <div className="flex-col--12 banner__content">
             <h1 className="banner__title">{banner.title}</h1>
             <ReactMarkdown className="banner__row" source={banner.content} />
-            <ButtonLink text={banner.button_text} href={banner.button_url} icon="arrow-right" />
           </div>
-          {banner.has_image && (
-            <div className="flex-col--6 flex-col--mobile--12">
-              <div className="banner__image">
-                <img src={`${apiBase}/settings/banner-image.png`} alt="Campaign logo" />
-              </div>
-            </div>
-          )}
         </div>
       </div>
     </div>
