ccd6ccbccba309645ab1024d01a2147fa0409f90
Haider Ismail <haider.i@icloud.com>
2021-05-27 12:07:46 +0100

    updated footer members button due to updates to the ButtonLink component to specify target attribute

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -84,7 +84,7 @@ const Footer: React.FunctionComponent<IProps> = ({ mobileMenu, cmsStore, uiStore
             </div>
 
             <div className="flex-col flex-col--6 flex-col--mobile--12 flex-col--tablet--12 footer__button">
-              <ButtonLink href="https://admin.connectedhounslow.uk" text="Members Area" />
+              <ButtonLink href="https://admin.connectedhounslow.uk" text="Members Area" target="_blank" />
               <img src={HounslowLogo} alt="London Borough of Hounslow" className="footer-hounslow-logo" />
             </div>
           </div>
